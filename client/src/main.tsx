c1f5ad376b88fb4ae7b3ff99436f9bffcb7be9f3
Alex Day <aday@hey.com>
2025-09-15 11:54:30 +0100

    prettier tailwind class sorter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,27 +12,26 @@ export const Main = () => {
   const handleClick = (cellRow: number, cellCol: number) => {
     const updatedBoard = board.map((row, rowIndex) =>
       row.map((cellValue, colIndex) =>
-        rowIndex === cellRow && colIndex === cellCol ? turn : cellValue
-      )
+        rowIndex === cellRow && colIndex === cellCol ? turn : cellValue,
+      ),
     );
     setBoard(updatedBoard);
     setTurn(turn === "X" ? "O" : "X");
   };
 
   return (
-    <div className="flex flex-col pt-10 items-center h-screen gap-10 bg-green text-white">
-      <h1 className="font-bold font-spruce text-2xl">Tic Tac Toe</h1>
-      <div className="flex flex-col gap-1 sm:gap-2 md:gap-3 w-2/5 max-w-screen-sm">
+    <div className="bg-green flex h-screen flex-col items-center gap-10 pt-10 text-white">
+      <h1 className="font-spruce text-2xl font-bold">Tic Tac Toe</h1>
+      <div className="flex w-2/5 max-w-screen-sm flex-col gap-1 sm:gap-2 md:gap-3">
         {board.map((row, rowIndex) => (
-          <div className="flex gap-1 sm:gap-2 md:gap-3 w-full">
+          <div className="flex w-full gap-1 sm:gap-2 md:gap-3">
             {row.map((column, colIndex) => {
               return (
                 <div
                   onClick={
                     !column ? () => handleClick(rowIndex, colIndex) : undefined
                   }
-                  className={`${!column && "cursor-pointer hover:bg-green-light"} border-2 border-gray-400 flex-1 aspect-square items-center justify-center text-2xl font-bold flex
-                  `}
+                  className={`${!column && "hover:bg-green-light cursor-pointer"} flex aspect-square flex-1 items-center justify-center border-2 border-gray-400 text-3xl font-bold md:text-5xl lg:text-8xl`}
                 >
                   {column}
                 </div>
